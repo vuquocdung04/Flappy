757828bdb687e49d390c1285b9222f1ac14829f5
Dung <vuquocdung204@gmail.com>
2025-02-10 17:05:02 +0700

    tile + spawnpoint

---
+++ Assets/_Data/Scripts/Tiles/TileSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : LoadAutoComponents
+{
+    [SerializeField] protected List<Transform> prefabs;
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadListPrefabs();
+    }
+
+    protected virtual void LoadListPrefabs()
+    {
+        Transform prefabs = transform.Find("Prefabs");
+        foreach (Transform prefab in prefabs)
+        {
+            this.prefabs.Add(prefab);
+        }
+        //this.HidePrefabs();
+    }
+
+    protected virtual void HidePrefabs()
+    {
+        foreach (Transform prefab in this.prefabs)
+        {
+            prefab.gameObject.SetActive(false);
+        }
+    }
+}
