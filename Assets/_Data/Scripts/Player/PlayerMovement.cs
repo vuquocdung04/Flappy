84023cb738249e4dd499c7082f4bedc0386ff2b0
Dung <vuquocdung204@gmail.com>
2025-02-10 15:54:50 +0700

    player movement + bg loop

---
+++ Assets/_Data/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerMovement : PlayerAbstract
+{
+    [SerializeField] protected float jumpForce = 5f;
+    [SerializeField] protected float rotateSpeed = 1f;
+
+
+    private void Update()
+    {
+        this.Moving();
+    }
+    private void FixedUpdate()
+    {
+        this.Rotating();
+    }
+
+    protected virtual void Moving()
+    {
+        if (InputManager.Instance.HandleInputMouseClick())
+        {
+            PlayerCtrl.Rigidbody2D.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    protected virtual void Rotating()
+    {
+        var bird = PlayerCtrl.Rigidbody2D.velocity.y * rotateSpeed;
+        this.transform.parent.rotation = Quaternion.Euler(0,0, bird * rotateSpeed);
+    }
+}
